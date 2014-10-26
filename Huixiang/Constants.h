0852e72f95c2543e30a5334b40b97c8a46a2983a
supersheep <supersheepxjy@gmail.com>
2014-10-26 17:01:21 +0800

    add global configs to Contstants.h

---
+++ Huixiang/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//
+
+#ifndef Constants_h
+#define Constants_h
+#endif
+
+#pragma mark - Instances
+#ifdef DEBUG
+#define DOMAIN_URL @"http://127.0.0.1:8080/api"
+#else
+#define DOMAIN_URL @"http://huixiang.im/api"
+#endif
